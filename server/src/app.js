9f78a48a40e230bdd81d6bf8ba4d40649a1fa6ca
Mithun <mithun@xdoto.io>
2023-11-21 17:58:02 +0530

    server project initial commit

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const app = require("./app");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+
+const server = http.createServer(app);
+
+async function startServer() {
+  server.listen(PORT, () => {
+    console.log(`Server Start On ${PORT}`);
+  });
+}
+
+startServer();
