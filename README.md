49f28cacb1bbf525edd1293cfe99e3818dff1338
Mithun <mithun@xdoto.io>
2023-11-30 14:33:57 +0530

    change redme File and add users Mysql file

--- README.md
+++ README.md
@@ -1,8 +1,66 @@
-# React + Vite
+# This is React js and Node js Project
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This App Contain simple CRUD task with login System using JWT authentication and use redux persist to save the LogUser Data to localStorage
 
-Currently, two official plugins are available:
+## Built With
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- React Js
+- Redux
+- Node Js
+- Express
+- MYSQL
+
+## Installation
+
+1. Clone the repo
+
+   ```sh
+   git clone https://github.com/XxCodoxX/Student_Registration_System-CRUD-
+   ```
+
+2. Install NPM packages
+
+   ```sh
+   npm run install
+   ```
+
+3. Create .env file in server Root directory
+4. Add thees keys to the server .env and use Your Config Data
+
+   ```sh
+   PORT = ''
+   MYSQL_HOST = ''
+   MYSQL_USER = ''
+   MYSQL_PASSWORD = ''
+   MYSQL_DB_NAME = ''
+   JWT_AUTH_TOKEN = ''
+   JWT_AUTH_REFRESH_TOKEN = ''
+   JWT_AUTH_TOKEN_EXPTIME = ''
+   JWT_AUTH_REFRESH_TOKEN_EXPTIME = ''
+   ```
+
+5. Create .env file in client Root directory
+6. Add thees keys to the client .env and use Your Config Data
+
+   ```sh
+   VITE_BASE_URL = ''
+   ```
+
+7. Import given `users` Table to the MYSQL
+   ```sh
+   User Name = 'test 01'
+   Password = '123'
+   ```
+8. Run the server
+
+   ```sh
+   cd ./server/
+   npm run watch
+   ```
+
+9. run the client
+
+   ```
+   cd ./client/
+   npm run dev
+   ```
---
+++ users.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2023 at 09:55 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `srs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `full_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `role` varchar(50) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `phoneNo` int(13) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userName`, `password`, `full_name`, `email`, `role`, `age`, `phoneNo`, `active`) VALUES
+(1, 'test 01', '$2b$10$DUWjop7OnnOGlbLdspHisu/Bqg8GUtmAFfFVnecGT/bcCZ5y94.P2', 'test User 01', 'test@test.com', 'admin', 15, 0118918578, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
